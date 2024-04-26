02fcf6db1b0829dc0ac5fbd6904608dafd28b2a7
Kun Lin Liu <78029945+kunlin1013@users.noreply.github.com>
2024-04-27 01:00:31 +0800

    init commit

---
+++ 0142.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *detectCycle(struct ListNode *head) {
+    if (head == NULL)
+        return NULL;
+    
+    struct ListNode *fast, *slow;
+    slow = fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            slow = head;
+            while (slow != fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
